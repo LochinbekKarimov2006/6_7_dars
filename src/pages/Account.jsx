576b95ac4f99f7dcbaacfee9a2375732eb088f5d
lochinbek <lochinbekk77@gmail.com>
2024-07-28 23:32:11 +0500

    xcfgvh

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,7 +3,7 @@ function Account() {
     let data= JSON.parse(localStorage.getItem("data"));
     console.log(data)
   return (
-    <div className='div-13'>
+    <div className='div-13 mt-10'>
         <div className='div-11'><img src={data.area} alt="" /></div>
         <div className='div-12'>
          <div><p className='p'>Name:</p><p className='p2'>{data.name}</p></div>
